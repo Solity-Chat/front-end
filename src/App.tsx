b9a1b5a4c40f4c0bc317f4f379a181e4f43eee50
Andrei Popescu <andreipopescu@Andreis-MacBook-Pro.local>
2020-04-07 12:37:02 +0300

    Created the main look for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+/**
+ * @constructor
+ * Generator for the navigation bar
+ */
+const Navigation = () => (
+  <div>
+    <nav className="navbar navbar-expand-sm bg-light">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/users">Users</Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+)
+
+/**
+ * Generate the switches for the main screen
+ * @constructor
+ */
+const Switches = () => (
+  <Switch>
+    <Route path="/about">
+      <h1>Hello</h1>
+    </Route>
+    <Route path="/users">
+      <h1>Hello 1</h1>
+    </Route>
+    <Route path="/">
+      <h1>Hello 2</h1>
+    </Route>
+  </Switch>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          {Navigation()}
+        </nav>
+        {Switches()}
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
